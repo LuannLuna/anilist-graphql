0b78a4805ae34d66d28833786779ae6ac585dcac
Luann Luna <luann.luna@thoughtworks.com>
2024-04-22 14:37:10 -0300

    created empty project

---
+++ AniList/AniList/AniListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniListApp.swift
+//  AniList
+//
+//  Created by Luann Marques Luna on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct AniListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
